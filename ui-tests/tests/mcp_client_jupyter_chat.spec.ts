5c578c9cd19f10443b3a06b5cd70cf4d1d288f0a
Inna <inna.copple@gmail.com>
2025-02-01 18:38:30 +0000

    fix jlpm run lint:check

--- ui-tests/tests/mcp_client_jupyter_chat.spec.ts
+++ ui-tests/tests/mcp_client_jupyter_chat.spec.ts
@@ -16,6 +16,8 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(s => s === 'JupyterLab extension mcp-client-jupyter-chat is activated!')
+    logs.filter(
+      s => s === 'JupyterLab extension mcp-client-jupyter-chat is activated!'
+    )
   ).toHaveLength(1);
 });
